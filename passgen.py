8eac534c134c192525ba30ccd0f76caf71e5da0e
Luigi Boschetti <taylorechristiansen@gmail.com>
2021-11-17 09:04:23 -0500

    Added Comments

---
+++ passgen.py
@@ -0,0 +1,8 @@
+import random #random module
+
+print("Whats up, its Tyrones password generator") #introductory message
+
+Length = int(input("Password Length?: ")) #gets input from the user
+Characters = "abcdefghijklmnopqrstuvwxyz0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ!@#$%^&*()" #random characters
+Password = "".join(random.sample(Characters, Length)) #takes the information held in characters variable and information held in length variable and makes a random password
+print(Password) #prints the password
