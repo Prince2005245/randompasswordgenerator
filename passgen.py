84ee07bad22cbfb496f40e4405c072ddfa6d3c06
Luigi Boschetti <taylorechristiansen@gmail.com>
2021-11-17 16:23:33 -0500

    Added Goodbye Message

--- passgen.py
+++ passgen.py
@@ -6,3 +6,4 @@ Length = int(input("Password Length?: ")) #gets input from the user
 Characters = "abcdefghijklmnopqrstuvwxyz0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ!@#$%^&*()" #random characters
 Password = "".join(random.sample(Characters, Length)) #takes the information held in characters variable and information held in length variable and makes a random password
 print(Password) #prints the password
+print("Thank you for using the program")
